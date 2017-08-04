8cfe43dd89923a1355e0027b8df44548e36f9c75
Sébastien Florent <sebastien.florent@qxlricardo.fr>
2017-08-04 13:54:53 +0200

    Add state and use localstorage * create state object * trigger on change * store changes in localstorage

--- src/index.js
+++ src/index.js
@@ -5,16 +5,52 @@ import { render } from 'react-dom';
 import './style/css/bootstrap.min.css';
 import './index.css';
 
+import { sampleText } from './sampleText';
+
+//Marked.js
+import marked from 'marked';
+
 class App extends React.Component {
+    //on crée un state dans lequel on importe le contenu du fichier sampleText
+    state = {
+        text: sampleText
+    };
+    componentWillMount() {
+        const localStorageText = localStorage.getItem('text');
+        if(localStorageText) {
+            this.setState({text:localStorageText})
+        }
+    }
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('text', nextState.text);
+    }
+    //à chaque changement dans le form, on met à jour le state
+    editText=(event) => {
+        const text = event.target.value;
+        this.setState({text})
+    }
+
+     //on affiche le rendu avec markdown / sanitize évite l'inclusion de html
+    renderText = (text) => {
+        const renderText = marked(text, {sanitize :true});
+        return { __html:renderText}
+    }
     render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6">
-                        <textarea></textarea>
+                        <textarea 
+                            rows="35" 
+                            className="form-control" 
+                            value={this.state.text}
+                            onChange={(e) => this.editText(e)}
+                            >
+                        </textarea>
                     </div>
                     <div className="col-sm-6">
                         <h1>Resultats</h1>
+                        <div dangerouslySetInnerHTML={this.renderText(this.state.text)}></div>
                     </div>
                 </div>
             </div>
